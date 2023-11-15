8c517996000b0344969fa616daea95fe1288f22e
kimskyyyy <hhhhh823@naver.com>
2023-11-15 14:46:31 +0900

    account 공통 응답 형식 적용

--- routes/account.js
+++ routes/account.js
@@ -2,25 +2,19 @@ var express = require("express");
 var router = express.Router();
 var account = require("../module/accountModule");
 
+const response = require("../util/response");
+
 /* POST 계정 인증 */
 router.post("/", function (req, res, next) {
-  // result = JSON.parse(account(req, res, next));
   result = account(req, res, next);
 
   console.log("result: " + result);
   console.log("result.token: " + result.token);
 
   if (result) {
-    res.status(200).send({
-      success: true,
-      message: "인증 성공",
-      data: result.token,
-    });
+    res.status(200).send(response.success(200, "인증 성공", result.token));
   } else {
-    res.status(500).send({
-      success: false,
-      message: "인증 실패",
-    });
+    res.status(500).send(response.fail(500, "인증 실패"));
   }
 });
 
