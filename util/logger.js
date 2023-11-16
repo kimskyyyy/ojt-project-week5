9eeb5107a6c25a51595fee682c71b95ee67b1cec
kimskyyyy <hhhhh823@naver.com>
2023-11-16 14:05:25 +0900

    winston 타임스탬프 한국 시간으로 설정

--- util/logger.js
+++ util/logger.js
@@ -1,4 +1,7 @@
 const winston = require("winston");
+const moment = require("moment-timezone");
+
+moment.tz.setDefault("Asia/Seoul"); // 한국 시간으로 설정
 
 const logger = winston.createLogger({
   // 출력 형식 정의
@@ -9,7 +12,9 @@ const logger = winston.createLogger({
   // 로그 형식 지정
   format: winston.format.combine(
     // combine을 사용하여 여러 형식을 조합할 것을 배열로 정의
-    winston.format.timestamp(), //  시간 출력
+    winston.format.timestamp({
+      format: moment().format("YYYY-MM-DD HH:mm:ss"), // 시간 형식 지정
+    }), //  시간 출력
     winston.format.colorize(), // 색상 출력
     winston.format.json(), // JSON 형식으로 출력
     winston.format.simple() // ${info.level}: ${info.message} 형식으로 출력
