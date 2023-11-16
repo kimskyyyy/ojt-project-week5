3cc4e36ba4b7e9e03cbcef84ad58f785e8749da5
kimskyyyy <hhhhh823@naver.com>
2023-11-16 16:22:22 +0900

    이메일 중복 예외처리

--- routes/users.js
+++ routes/users.js
@@ -8,7 +8,7 @@ const response = require("../util/response");
 
 /* GET 계정 전체 조회 */
 router.get("/", function (req, res, next) {
-  logging.info("GET: 계정 전체 조회");
+  logging.info("GET/ 계정 전체 조회");
   const users = UserRealm.objects("User").sorted("date", true);
 
   res.send({
@@ -20,7 +20,7 @@ router.get("/", function (req, res, next) {
 /* POST 계정 추가. */
 // todo: 이메일 중복체크
 router.post("/", function (req, res, next) {
-  logging.info("POST: 계정 추가", { body: req.body });
+  logging.info("POST/ 계정 추가", { body: req.body });
 
   var email = req.body.email;
   var password = req.body.password;
@@ -28,19 +28,25 @@ router.post("/", function (req, res, next) {
   var tel = req.body.tel;
 
   try {
-    UserRealm.write(() => {
-      UserRealm.create("User", {
-        email: email,
-        password: password,
-        name: name,
-        tel: tel,
-        toeken: "default",
-        date: new Date(),
-        tokenExp: new Date(),
+    // 이메일 중복체크
+    const existUser = account.findUserByEmail(email);
+    if (existUser) {
+      logging.error("이메일 중복", { email: email });
+      res.status(409).send(response.fail(409, "이메일 중복")); // 409: Conflict
+    } else {
+      UserRealm.write(() => {
+        UserRealm.create("User", {
+          email: email,
+          password: password,
+          name: name,
+          tel: tel,
+          toeken: "default",
+          date: new Date(),
+          tokenExp: new Date(),
+        });
       });
-    });
-
-    res.status(201).send(response.success(201, "계정 추가 성공"));
+      res.status(201).send(response.success(201, "계정 추가 성공"));
+    }
   } catch (error) {
     logging.error("계정 추가 실패", { error: error });
     res.status(500).send(response.fail(500, "계정 추가 실패"));
@@ -49,7 +55,7 @@ router.post("/", function (req, res, next) {
 
 /* DELETE 계정 삭제 */
 router.delete("/:email", function (req, res, next) {
-  logging.info("DELETE: 계정 삭제", { email: req.params.email });
+  logging.info("DELETE/ 계정 삭제", { email: req.params.email });
   const user = account.findUserByEmail(req.params.email);
 
   try {
@@ -68,7 +74,7 @@ router.delete("/:email", function (req, res, next) {
 
 /* PUT 비밀번호 변경. */
 router.put("/:email", function (req, res, next) {
-  logging.info("PUT: 비밀번호 변경", { email: req.params.email });
+  logging.info("PUT/ 비밀번호 변경", { email: req.params.email });
   const user = account.findUserByEmail(req.params.email);
   const password = account.findPassword(user.password, req.body.inputPassword);
 
@@ -88,7 +94,7 @@ router.put("/:email", function (req, res, next) {
 
 /* POST 로그인 */
 router.post("/login", function (req, res, next) {
-  logging.info("로그인 요청", { email: req.body.email });
+  logging.info("POST/ 로그인 요청", { email: req.body.email });
   result = account.account(req, res, next);
 
   if (result) {
